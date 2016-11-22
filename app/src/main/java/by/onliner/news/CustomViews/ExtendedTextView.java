8d5e6e36cf41e03bce0022ef5c6e9506035c93aa
Orion <orion.gm@ya.ru>
2016-11-22 20:30:44 +0300

    continue work. Changed fonts for some TextView.

---
+++ app/src/main/java/by/onliner/news/CustomViews/ExtendedTextView.java
@@ -0,0 +1,56 @@
+package by.onliner.news.CustomViews;
+
+/**
+ * Created by orion on 22.11.16.
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+import by.onliner.news.R;
+
+/**
+ * Created by orion on 22.11.16.
+ */
+
+class ExtendedTextView extends TextView {
+    private static final String TAG = "ExtentedTextView";
+
+    public ExtendedTextView(Context context) {
+        super(context);
+    }
+
+    public ExtendedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public ExtendedTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.ExtendedTextView);
+        String customFont = a.getString(R.styleable.ExtendedTextView_fontAsset);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface tf;
+        try {
+            tf = Typeface.createFromAsset(ctx.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not get typeface: "+e.getMessage());
+            return false;
+        }
+
+        setTypeface(tf);
+        return true;
+    }
+}
